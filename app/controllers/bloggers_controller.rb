494ef7464fe2a2d18432793971cb7bc939ac2929
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-20 11:04:47 -0700

    add routes for linking to new blogger form

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -3,4 +3,18 @@ class BloggersController < ApplicationController
     @bloggers = Blogger.all
     render :index
   end
+
+  def new
+    @blogger = Blogger.new
+    render :new
+  end
+
+  def create
+    @blogger = Blogger.new(params[:blogger])
+    if @blogger.save
+      redirect_to bloggers_path
+    else
+      render :new
+    end
+  end
 end
