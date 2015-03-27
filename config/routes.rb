df78eb9a44ec017a1b37204170546cc404efa5df
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-26 21:30:45 -0700

    add routing to new post page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
   root to: "users#index"
+
+  devise_for :users
+  resources :users, only: :show
+  resources :posts, except: :index
 end
