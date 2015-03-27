9da9be461dcd8b349f02808fc07415769d95117f
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-26 21:48:10 -0700

    add form for adding new post with correct routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
 
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  #   @post = Post.new
+  # end
+
+  # def create
+  #   @user = User.find(current_user.id)
+  #   @post = @user.posts.new(posting_params)
+  #   if
 
 end
