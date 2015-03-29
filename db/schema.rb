9c03fcdef5aa57dab491470185a9b27dca210edf
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-28 23:42:19 -0700

    add comments table in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327052940) do
+ActiveRecord::Schema.define(version: 20150329063432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", force: :cascade do |t|
-    t.string  "title"
-    t.text    "body"
-    t.integer "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
