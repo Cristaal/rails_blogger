c2194102901be155a97cab50c6cfc1aed4644209
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-29 21:18:36 -0700

    add integration spec for updating blog post

--- spec/features/add_blog_post_spec.rb
+++ spec/features/add_blog_post_spec.rb
@@ -14,4 +14,23 @@ describe "the add a blog post process" do
     click_on "Create Post"
     expect(page).to have_content "Post Successfully Added!"
   end
+
+  it "updates a blog post" do
+    visit root_path
+    user = FactoryGirl.create(:user)
+    click_on "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    click_on "New Entry"
+    fill_in "Title", with: Faker::Lorem.sentence(rand(2..10)).chomp('.')
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    click_on "Create Post"
+    click_on "Your Blog Posts"
+    click_on "Edit Post"
+    fill_in "Title", with: Faker::Lorem.sentence(rand(2..10)).chomp('.')
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    click_on "Update Post"
+    expect(page).to have_content "Post Successfully Updated"
+  end
 end
