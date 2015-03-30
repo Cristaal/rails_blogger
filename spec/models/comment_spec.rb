8eae46a0b5e9452b9ffb5ad54bad80274429c611
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-29 20:12:30 -0700

    add validation spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Comment do
   it { should validate_presence_of :body }
   it { should belong_to :post}
+  it { should belong_to :user}
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,5 @@ describe Post do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should have_many :comments }
+  it { should belong_to :user }
 end
