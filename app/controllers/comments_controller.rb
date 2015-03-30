92c18e9b8a905596ac0af439c998522ca8598ea2
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-29 19:43:47 -0700

    add delete and edit functions for comments on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,30 @@ class CommentsController < ApplicationController
     end
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:success] = "Comment Successfully Updated"
+      redirect_to post_path(@comment.post)
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:danger] = "Comment Successfully Deleted!"
+    redirect_to posts_path(@posts)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body, :user_id, :post_id)
