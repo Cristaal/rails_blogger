66f9f236cdb329d2695551425386cb816b3b848f
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-29 19:20:52 -0700

    display posted comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,26 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
+
   def index
   end
 
   def new
-    @comment = Comment.new
+    @comment = Comment.new(post_id: params[:post_id])
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:success] = "Your Comment Has Been Added!"
+      redirect_to post_path(@comment.post)
+    else
+      flash[:danger] = "There was a problem creating your comment, please try again."
+      render :new
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body, :user_id, :post_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   resources :users, only: :show
-  resources :posts do
-    resources :comments
-  end
+  resources :posts
+  resources :comments
 end
