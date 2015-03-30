e43f6582aad1cef3aa79ba12e508ea9d4b5c3828
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-29 22:40:59 -0700

    add webdriver to integration tests

--- spec/features/add_blog_post_spec.rb
+++ spec/features/add_blog_post_spec.rb
@@ -26,11 +26,24 @@ describe "the add a blog post process" do
     fill_in "Title", with: Faker::Lorem.sentence(rand(2..10)).chomp('.')
     fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
     click_on "Create Post"
-    click_on "Your Blog Posts"
+    click_on "Blog Posts"
     click_on "Edit Post"
     fill_in "Title", with: Faker::Lorem.sentence(rand(2..10)).chomp('.')
     fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
     click_on "Update Post"
     expect(page).to have_content "Post Successfully Updated"
+  end
+
+  it "deletes a blog post", js: true do
+    user = FactoryGirl.create(:user, password: "password", password_confirmation: "password")
+    visit root_path
+    click_on "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    click_on "Your Blog Posts"
+    click_on "Delete Post"
+    page.driver.browser.switch_to.alert.accept
+    expect(page).to have_content "Post Successfully Deleted!"
   end
 end
