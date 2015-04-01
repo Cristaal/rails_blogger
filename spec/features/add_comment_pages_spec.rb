8cdb8beb5cb750f43886151b90430da8fd26dc8f
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-31 21:09:00 -0700

    add work-in-progress spec for updating a comment

--- spec/features/add_comment_pages_spec.rb
+++ spec/features/add_comment_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "the add a comment to blog post process" do
-  it "adds a comment to an existing blog post" do 
+  it "adds a comment to an existing blog post" do
     visit root_path
     user = FactoryGirl.create(:user)
     click_on "Login"
@@ -19,4 +19,43 @@ describe "the add a comment to blog post process" do
     click_button "Create Comment"
     expect(page).to have_content "Your Comment Has Been Added!"
   end
+
+  it "updates a comment" do
+    visit root_path
+    user = FactoryGirl.create(:user)
+    click_on "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    click_on "New Entry"
+    title = Faker::Lorem.sentence(rand(2..10)).chomp('.')
+    fill_in "Title", with: title
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    click_on "Create Post"
+    click_link "#{title}"
+    click_on "Add a Comment"
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    click_button "Create Comment"
+    click_link "Edit Comment"
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    expect(page).to have_content "Comment Successfully Updated"
+  end
+
+  it "deletes a blog post", js: true do
+    visit root_path
+    user = FactoryGirl.create(:user)
+    click_on "Login"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button "Log in"
+    click_on "New Entry"
+    fill_in "Title", with: Faker::Lorem.sentence(rand(2..10)).chomp('.')
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    click_button "Create Post"
+    click_on "Your Blog Posts"
+    first('.btn').click
+    page.driver.browser.switch_to.alert.accept
+    expect(page).to have_content "Post Successfully Deleted!"
+  end
+
 end
