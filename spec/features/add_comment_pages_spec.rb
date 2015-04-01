3fe0703f4fedfded513b614f2ad40809bb47b2d5
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-31 20:47:57 -0700

    add spec for add comment to post. includes using string interpolation to use the title variable inside of the click_link string

--- spec/features/add_comment_pages_spec.rb
+++ spec/features/add_comment_pages_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 describe "the add a comment to blog post process" do
-  it "adds a comment to an existing blog post" do
+  it "adds a comment to an existing blog post", js: true do
+    visit root_path
+    user = FactoryGirl.create(:user)
+    click_on "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    click_on "New Entry"
+    title = Faker::Lorem.sentence(rand(2..10)).chomp('.')
+    fill_in "Title", with: title
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    click_on "Create Post"
+    click_link "#{title}"
+    click_on "Add a Comment"
+    fill_in "Body", with: Faker::Lorem.words(rand(2..10)).join(' ')
+    click_button "Create Comment"
+    expect(page).to have_content "Your Comment Has Been Added!"
   end
 end
